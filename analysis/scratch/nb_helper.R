2d3fd7794eaede595cee23add21a273fb1343de0
Wylie Timmerman <wtimmerman@foursquareitp.com>
2021-06-11 08:59:24 -0400

    renv update, along with cleanup of certain bad R files that were preventing dependency detection

--- analysis/scratch/nb_helper.R
+++ analysis/scratch/nb_helper.R
@@ -8,11 +8,11 @@ magic <-
     & any(stop_area_state == c("t_l_addl"))
   )
     
-    all(any(stop_area_state %in% c("t_decel_phase",
-                                        "t_l_initial",
-                                        "t_stop1",
-                                        "t_l_addl",
-                                        "t_accel_phase"))))
+    # all(any(stop_area_state %in% c("t_decel_phase",
+    #                                     "t_l_initial",
+    #                                     "t_stop1",
+    #                                     "t_l_addl",
+    #                                     "t_accel_phase"))))
 
 
 candidates <- magic %>%
