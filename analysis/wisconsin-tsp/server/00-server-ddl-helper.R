2df6425d3c5bb384080a1d67d4ba0cd21376b5de
Wylie Timmerman <wtimmerman@foursquareitp.com>
2021-06-14 09:00:06 -0400

    Pushes processed rawnav data with R, after running into roadblocks with Python packages

---
+++ analysis/wisconsin-tsp/server/00-server-ddl-helper.R
@@ -0,0 +1,194 @@
+# NOTE: SQL statements were not really playing well in Python, so we are 
+# doing this quickly in R
+# sorry for the copied code!
+library(DBI)
+library(tidyverse)
+library(RPostgres)
+library(dotenv)
+library(arrow)
+library(glue)
+
+
+path_data <-
+  "C:/OD/Foursquare ITP/Foursquare ITP SharePoint Site - Shared Documents/WMATA Datamart Program Support/Task 3 - Bus Priority/Data/02-Processed"
+load_dot_env(file = ".env")
+
+conn <- 
+  DBI::dbConnect(
+    RPostgres::Postgres(),
+    user = Sys.getenv('pg_user'), 
+    password = Sys.getenv('pg_pass'), 
+    dbname = Sys.getenv('pg_db'), 
+    host = Sys.getenv('pg_host'),
+    port = Sys.getenv('pg_port'),
+    sslmode = Sys.getenv('pg_sslmode'),
+  )
+
+# Rawdata -----------------------------------------------------------------
+
+# we are running out of time, so instead of defining tables in advance,
+# we are just pushing the first one and then appending the others after.
+filelist <- 
+  list.files(path = file.path(path_data,"rawnav_data_202102.parquet"),
+             recursive = TRUE,
+             full.names = TRUE)
+
+rawnav_data_202102 <- 
+  map_dfr(filelist,
+          ~ arrow::read_parquet(.x))
+
+dbWriteTable(
+  conn,
+  name = Id(schema = Sys.getenv('pg_schema'), table = "rawnav"),
+  value = rawnav_data_202102,
+  overwrite = FALSE, 
+  append = TRUE,
+  row.names = FALSE
+)
+
+# SEt primary key
+statement <-
+  glue_sql("ALTER TABLE {`schema`}.{`table`} OWNER TO {`groupname`}",
+           schema = Sys.getenv("pg_schema"),
+           table = "rawnav",
+           groupname = "datamart_group",
+           .con = conn)
+
+dbExecute(conn, statement)
+
+# Remainder 
+
+thelist <- 
+  c(
+    "rawnav_data_202103.parquet",
+    "rawnav_data_202104.parquet",
+    "rawnav_data_202105.parquet"
+  )
+
+for (theitem in thelist) {
+  filelist <- 
+    list.files(path = file.path(path_data,theitem),
+               recursive = TRUE,
+               full.names = TRUE)
+  
+  rawnav_data_forup <- 
+    map_dfr(filelist,
+            ~ arrow::read_parquet(.x))
+  
+  dbWriteTable(
+    conn,
+    name = Id(schema = Sys.getenv('pg_schema'), table = "rawnav"),
+    value = rawnav_data_forup,
+    overwrite = FALSE, 
+    append = TRUE,
+    row.names = FALSE
+  )
+}
+
+#update the index
+statement <-
+  glue_sql('CREATE INDEX {`paste0(`table`,`index`)`} 
+           ON {`schema`}.{`table`} ("filename", "index_run_start")',
+           schema = Sys.getenv("pg_schema"),
+           table = "rawnav",
+           index = "fileindex",
+           .con = conn)
+
+dbExecute(conn, statement)
+
+statement <-
+  glue_sql('CREATE INDEX {`paste0(`table`,`index`)`} 
+           ON {`schema`}.{`table`} ("route_pattern")',
+           schema = Sys.getenv("pg_schema"),
+           table = "rawnav",
+           index = "fileindex",
+           .con = conn)
+
+dbExecute(conn, statement)
+
+
+# Summary -----------------------------------------------------------------
+
+
+filelist <- 
+  list.files(path = file.path(path_data,"rawnav_summary_202102.parquet"),
+             recursive = TRUE,
+             full.names = TRUE)
+
+rawnav_summary_202102 <- 
+  map_dfr(filelist,
+          ~ arrow::read_parquet(.x))
+
+dbWriteTable(
+  conn,
+  name = Id(schema = Sys.getenv('pg_schema'), table = "rawnav_summary"),
+  value = rawnav_summary_202102,
+  overwrite = FALSE, 
+  append = TRUE,
+  row.names = FALSE
+)
+
+statement <-
+  glue_sql("ALTER TABLE {`schema`}.{`table`} OWNER TO {`groupname`}",
+           schema = Sys.getenv("pg_schema"),
+           table = "rawnav_summary",
+           groupname = "datamart_group",
+           .con = conn)
+
+dbExecute(conn, statement)
+
+
+
+thelist <- 
+  c(
+    "rawnav_summary_202103.parquet",
+    "rawnav_summary_202104.parquet",
+    "rawnav_summary_202105.parquet"
+  )
+
+
+for (theitem in thelist) {
+  filelist <- 
+    list.files(path = file.path(path_data,theitem),
+               recursive = TRUE,
+               full.names = TRUE)
+  
+  rawnav_data_forup <- 
+    map_dfr(filelist,
+            ~ arrow::read_parquet(.x))
+  
+  dbWriteTable(
+    conn,
+    name = Id(schema = Sys.getenv('pg_schema'), table = "rawnav_summary"),
+    value = rawnav_data_forup,
+    overwrite = FALSE, 
+    append = TRUE,
+    row.names = FALSE
+  )
+}
+
+#update the index
+statement <-
+  glue_sql('CREATE INDEX {`paste0(`table`,`index`)`} 
+           ON {`schema`}.{`table`} ("filename", "index_run_start")',
+           schema = Sys.getenv("pg_schema"),
+           table = "rawnav_summary",
+           index = "fileindex",
+           .con = conn)
+
+dbExecute(conn, statement)
+
+statement <-
+  glue_sql('CREATE INDEX {`paste0(`table`,`index`)`} 
+           ON {`schema`}.{`table`} ("route")',
+           schema = Sys.getenv("pg_schema"),
+           table = "rawnav_summary",
+           index = "fileindex",
+           .con = conn)
+
+dbExecute(conn, statement)
+
+
+dbDisconnect(conn)
+
+
