452d99ae92e8bb9b3da0a9d8142a72501028e745
Wylie Timmerman <wtimmerman@foursquareitp.com>
2021-09-21 10:12:06 -0400

    an additional stab at visualizing 2017 v 2019 before meeting

--- analysis/map-matching/visualize-meili-test-rawnav.R
+++ analysis/map-matching/visualize-meili-test-rawnav.R
@@ -12,44 +12,75 @@ path_data <-
     "01-Interim"
   )
 
-rawnav_matched <-
+rawnav <-
   read_csv(
-    file.path(path_data,"rawnav_mapmatch_matched.csv")
-  ) %>%
-  st_as_sf(
-    .,
-    coords = c("lon", "lat"),
-    crs = 4326L, #WGS84
-    agr = "constant"
-  )
-      
+    file.path(path_data,"rawnav_matched.csv"),
+    n_max = 100000
+  ) 
 
-rawnav_in <-
-  read_csv(
-    file.path(path_data,"rawnav_mapmatch_in.csv")
-  ) %>%
+rawnav_matched <-
+  rawnav %>%
   st_as_sf(
     .,
-    coords = c("lon", "lat"),
+    coords = c("longmatch", "latmatch"),
     crs = 4326L, #WGS84
     agr = "constant"
   )
+
+rawnav_og <-
+  rawnav %>%
+  select(
+    filename,
+    index_run_start,
+    index_loc,
+    long,
+    lat
+  ) %>%
+  st_as_sf(
+    .,
+    coords = c("long", "lat"),
+    crs = 4326L, #WGS84
+    agr = "constant"
+  )
+
+thetripfile <- "rawnav02205171017.txt"
+thetripidx <- 1827
+
+rawnav_og_ti <-
+  rawnav_og %>%
+  filter(
+    filename == thetripfile,
+    index_run_start == thetripidx
+  )
       
+rawnav_matched_ti <-
+  rawnav_matched %>%
+  filter(
+    filename == thetripfile,
+    index_run_start == thetripidx
+  )
+
 # compare
-mapview(rawnav_in, col.regions = "blue") + mapview(rawnav_matched, col.regions = "red")
+mapview(rawnav_og_ti, col.regions = "grey80", alpha = 0.5, layer.name = "Original") +
+  mapview(rawnav_matched_ti, col.regions = "red", layer.name = "Matched")
 
 # look at size of miss
+mapview(rawnav_og_ti, col.regions = "grey80", alpha = 0.5, layer.name = "Original") + 
+  mapview(rawnav_matched_ti, zcol = "distance_from_trace_point", layer.name = "Dist. from Orig.")
 
-mapview(rawnav_in, col.regions = "grey80", alpha = 0.5) + 
-  mapview(rawnav_matched, zcol = "distance_from_trace_point")
+
+# edge index
+mapview(rawnav_matched_ti, zcol = "street_names")
+
+
+mapview(rawnav_matched_ti, zcol = "way_id")
 
 
 # distance along edge value
 # The distance along the associated edge for this matched point. For example, if the matched point is halfway along the edge then the value would be 0.5. This value will not exist if this point was unmatched.
-mapview(rawnav_matched, zcol = "distance_along_edge")
+mapview(rawnav_matched_ti, zcol = "distance_along_edge")
 
+# The distance along the associated edge for this matched point. For example, if the matched point is halfway along the edge then the value would be 0.5. This value will not exist if this point was unmatched.
+mapview(rawnav_matched_ti, zcol = "end_heading", layer.name = "street heading")
 
-# edge index
-# 
-mapview(rawnav_matched, zcol = "edge_index")
 
