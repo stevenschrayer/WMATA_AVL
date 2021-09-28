217bd25ad2e3254b38a68d416ef188dfb52037c8
Wylie Timmerman <wtimmerman@foursquareitp.com>
2021-09-28 11:01:29 -0400

    adds scratch scripts for debugging map matching

--- analysis/map-matching/meili-test-mapflip.R
+++ analysis/map-matching/meili-test-mapflip.R
@@ -1,20 +1,58 @@
+
+library(tidyverse)
+library(sf)
+library(mapview)
+
+path_sp <- "C:/OD/Foursquare ITP/Projects - WMATA Datamart"
+
+path_data <-
+  file.path(
+    path_sp,
+    "Task 3 - Bus Priority",
+    "Data",
+    "01-Interim"
+  )
+
+rawnav <-
+  read_csv(
+    file.path(path_data,"route43_decomp_match.csv")
+  )
+
+
+
+
 preview_match <-
   function(
     atripfile,
-    atripidx
+    atripidx,
+    zcol = "index_loc"
   ){
-    rawnav_og_ti <-
-      rawnav_og %>%
-      filter(
-        filename == atripfile,
-        index_run_start == atripidx
-      )
     
     rawnav_matched_ti <-
-      rawnav_matched %>%
+      rawnav %>%
       filter(
         filename == atripfile,
         index_run_start == atripidx
+      ) %>%
+      st_as_sf(
+        .,
+        coords = c("longmatch", "latmatch"),
+        crs = 4326L, #WGS84
+        agr = "constant",
+        remove = FALSE
+      )
+    
+    rawnav_og_ti <-
+      rawnav %>%
+      filter(
+        filename == atripfile,
+        index_run_start == atripidx
+      ) %>%
+      st_as_sf(
+        .,
+        coords = c("long", "lat"),
+        crs = 4326L, #WGS84
+        agr = "constant"
       )
     
     mapview(
@@ -25,9 +63,9 @@ preview_match <-
     ) +
       mapview(
         rawnav_matched_ti,
-        col.regions = "red",
         layer.name = "Matched",
-        alpha = 0
+        alpha = 0,
+        zcol = zcol
       )
     
     
@@ -43,23 +81,68 @@ preview_match(
   atripidx = 5735
 )
 
-#starts at 6469 in original file
-rawnav %>%
-  filter(
-    filename == "rawnav02547171017.txt",
-    index_run_start == 5735
-  ) %>%
-  View()
+preview_match(
+  atripfile = "rawnav02516171011.txt",
+  atripidx = 4260,
+  zcol = "stop_id_group"
+)
+
+
+preview_match(
+  atripfile = "rawnav02527171015.txt",
+  atripidx = 5378,
+  zcol = "stop_id_group"
+)
 
 rawnav %>%
   filter(
-    filename == "rawnav02547171017.txt",
-    index_run_start == 5735
+    filename == "rawnav02527171015.txt",
+    index_run_start == 5378,
   ) %>%
+  View()
+
+# this one in the same file
+preview_match(
+  atripfile = "rawnav02527171015.txt",
+  atripidx = 6475,
+  zcol = "door_state"
+)
+# even after redoing the match, still looks bad
+testrematch <-
+  read_csv("testrematch.csv")
+
+testrematch <-
+  testrematch %>%
   st_as_sf(
     .,
-    coords = c("long", "lat"),
+    coords = c("longmatch", "latmatch"),
     crs = 4326L, #WGS84
     agr = "constant"
-  ) %>%
-  mapview::mapview(zcol = "odom_ft_og")
+  )
+    
+
+preview_match(
+  atripfile = "rawnav02531171013.txt",
+  atripidx = 12892,
+  zcol = "trip_seg"
+)
+
+rawnav02533171018.txt
+22465
+
+preview_match(
+  atripfile = "rawnav02531171013.txt",
+  atripidx = 12892,
+  zcol = "trip_seg"
+)
+
+anothermatch <-
+  read_csv("another43.csv") %>%
+  st_as_sf(
+    .,
+    coords = c("longmatch", "latmatch"),
+    crs = 4326L, #WGS84
+    agr = "constant"
+  )
+
+mapview::mapview(anothermatch, zcol = "index_loc")
